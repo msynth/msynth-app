b6181bb1b5a603dc678bf771787f4df98e894f97
Steven Miyakawa <steven.miyakawa@here.com>
2017-08-08 01:25:52 -0700

    Update God mode to have dynamic text on buttons

--- components/God.js
+++ components/God.js
@@ -2,72 +2,54 @@
 import React, {PureComponent} from 'react'
 import {View, Image, Text, StyleSheet, TouchableOpacity, Linking, Platform, TouchableHighlight} from 'react-native'
 import {logo} from '../libs/Images'
-import sound from '../libs/sound/Sound'
+import publish from '../libs/PubNub'
+import pubnubClient from '../libs/pubnubClient'
+
+const DEFAULTS = ['Sample 1', 'Sample 2', 'Sample 3', 'Sample 4', 'Sample 5'];
 
 export default class God extends PureComponent {
-  constructor(props){
-    super(props);
+
+  constructor () {
+    super();
+
+    this.state = {
+      textForButtons: DEFAULTS
+    }
   }
 
-  bassDrop0() {
-    sound.soundSend('bassDrop0')
-  }
-  bassDrop1() {
-    sound.soundSend('bassDrop1')
-  }
-  bassDrop2() {
-    sound.soundSend('bassDrop2')
-  }
-  bassDrop3() {
-    sound.soundSend('bassDrop3')
-  }
-  bassDrop4() {
-    sound.soundSend('bassDrop4')
+  componentWillMount () {
+    pubnubClient.addListener({
+      message: (message) => {
+        this.setState({ textForButtons: message.message })
+      }
+    });
+
+    pubnubClient.subscribe({
+      channels: ['update_samples']
+    });
   }
 
-
-  render(): * {
-    const buttonLabel = 'bass drop'
-
-    return <View style={styles.wrapper}>
-      <Image source={logo} style={styles.logo} />
-      <TouchableOpacity style={styles.button}>
-        <TouchableHighlight underlayColor='transparent' onPress={() => this.bassDrop0()}>
-          {/* conditional button style changing based on the prop. via: http://www.terrydiederich.com/changing-style-as-state-changes-in-react-native/ */}
-          <View style={styles.buttonBoxStart}>
-            <Text style={styles.buttonLabelStart}>Lyft</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight underlayColor='transparent' onPress={() => this.bassDrop1()}>
-          {/* conditional button style changing based on the prop. via: http://www.terrydiederich.com/changing-style-as-state-changes-in-react-native/ */}
-          <View style={styles.buttonBoxStart}>
-            <Text style={styles.buttonLabelStart}>Uber</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight underlayColor='transparent' onPress={() => this.bassDrop2()}>
-          {/* conditional button style changing based on the prop. via: http://www.terrydiederich.com/changing-style-as-state-changes-in-react-native/ */}
-          <View style={styles.buttonBoxStart}>
-            <Text style={styles.buttonLabelStart}>Outside Lands</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight underlayColor='transparent' onPress={() => this.bassDrop3()}>
-          {/* conditional button style changing based on the prop. via: http://www.terrydiederich.com/changing-style-as-state-changes-in-react-native/ */}
-          <View style={styles.buttonBoxStart}>
-            <Text style={styles.buttonLabelStart}>Horn</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight underlayColor='transparent' onPress={() => this.bassDrop4()}>
-          {/* conditional button style changing based on the prop. via: http://www.terrydiederich.com/changing-style-as-state-changes-in-react-native/ */}
-          <View style={styles.buttonBoxStart}>
-            <Text style={styles.buttonLabelStart}>Drop</Text>
-          </View>
-        </TouchableHighlight>
-      </TouchableOpacity>
-    </View>
+  publishSound (sound) {
+    publish('artist_mode', sound);
   }
 
-
-
+  render () {
+    return (
+      <View style={styles.wrapper}>
+        <Image source={logo} style={styles.logo} />
+        <TouchableOpacity style={styles.button}>
+          {this.state.textForButtons.map((text, i) => (
+            <TouchableHighlight underlayColor='transparent' onPress={() => this.publishSound(`bassDrop${i}`)} key={`button-${i}`}>
+              {/* conditional button style changing based on the prop. via: http://www.terrydiederich.com/changing-style-as-state-changes-in-react-native/ */}
+              <View style={styles.buttonBoxStart}>
+                <Text style={styles.buttonLabelStart}>{text}</Text>
+              </View>
+            </TouchableHighlight>
+          ))}
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 let styles = StyleSheet.create({
